4f01593be320312bd544b6fdc990fd12d55b4c27
Shinmaruu <bdzhou@iu.edu>
2023-09-28 23:28:39 -0400

    Issues with Runtime
    Had some issues with the actual running of the game, too tired, will fix later

--- 3D Tutorial - Zhou/Assets/Scripts/GameSetup.cs
+++ 3D Tutorial - Zhou/Assets/Scripts/GameSetup.cs
@@ -10,14 +10,14 @@ public class GameSetup : MonoBehaviour
     float ballRadius;
     float ballDiameter;
 
-    [SerializeField] GameObject = ballPrefab;
+    [SerializeField] GameObject ballPrefab;
     [SerializeField] Transform cueBallPosition;
     [SerializeField] Transform headBallPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        ballRadius = ballPrefab.GetComponenet<SphereCollider>().radius = 100f;
+        ballRadius = ballPrefab.GetComponent<SphereCollider>().radius = 100f;
         ballDiameter = ballRadius * 2f;
         PlaceAllBalls();
     }
@@ -30,13 +30,13 @@ public class GameSetup : MonoBehaviour
 
     void PlaceCueBall()
     {
-        GameObject ball = Instatiate(ballPrefab, cueBallPosition, Quaternion.identity);
+        GameObject ball = Instantiate(ballPrefab, cueBallPosition.position, Quaternion.identity);
         ball.GetComponent<Ball>().MakeCueBall();
     }
 
     void PlaceEightBall(Vector3 position)
     {
-        GameObject ball = Instatiate(ballPrefab, position, Quaternion.identity);
+        GameObject ball = Instantiate(ballPrefab, position, Quaternion.identity);
         ball.GetComponent<Ball>().MakeEightBall();
     }
 
@@ -62,5 +62,47 @@ public class GameSetup : MonoBehaviour
             blueBallsRemaining--;
         }
 
-
+        // outer loop is 5 rows
+        for (int i = 0; i < 5; i++)
+        {
+            // inner loop are the balls in each row
+            for (int j = 0; i < NumInThisRow; j++)
+            {
+                // check to see if 8 ball spot
+                if (i == 2 && j == 1)
+                {
+                    PlaceEightBall(currentPosition);
+                }
+                //if red and blue ball remaining, randomly choose and place
+                else if (redBallsRemaining > 0 && blueBallsRemaining > 0)
+                {
+                    rand = Random.Range(0, 2);
+                    if (rand == 0)
+                    {
+                        PlaceRedBall(currentPosition);
+                    }
+                    else
+                    {
+                        PlaceBlueBall(currentPosition);
+                    }
+                }
+                // if only red balls are left, place one
+                else if (redBallsRemaining > 0)
+                {
+                    PlaceRedBall(currentPosition);
+                }
+                // otherwise, place blue ball
+                else
+                {
+                    PlaceBlueBall(currentPosition);
+                }
+                // move current position for the next ball in this row to the right
+                currentPosition += new Vector3(1, 0, 0).normalized * ballDiameter;
+            }
+            // once all the balls in the row have been placed, move to the next row
+            firstInRowPosition += new Vector3(-1, 0, -1).normalized * ballDiameter;
+            currentPosition = firstInRowPosition;
+            NumInThisRow++;
+        }
     }
+}
